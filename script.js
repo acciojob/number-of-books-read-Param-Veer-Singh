ebe801122dac1bf3a10889f28f6f83101b98c33c
Param-Veer-Singh <122340319+Param-Veer-Singh@users.noreply.github.com>
2023-10-11 17:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
-
-const numberOfBooksRead = () => {
-  // write your code here
-};
-
-// Do not change the code below
-
-alert(numberOfBooksRead());
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const numberOfBooksRead = () => {
+    // write your code here
+      let count = 0;
+      for (let index = 0; index < library.length; index++) {
+          if(library[i].readingStatus == true)count++;
+          return count;
+      }
+  };
+  
+  // Do not change the code below
+  
+  alert(numberOfBooksRead());
+  
